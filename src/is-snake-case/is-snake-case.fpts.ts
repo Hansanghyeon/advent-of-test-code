681dc2de8dbe0216d3207cf824e06d922b7eb0b4
Hansanghyeon <999@hyeon.pro>
2024-01-10 02:37:09 +0000

    feat: fpts용 fromSnakeCase 추가

---
+++ src/is-snake-case/is-snake-case.fpts.ts
@@ -0,0 +1,11 @@
+import * as O from 'fp-ts/Option'
+
+// O.fromNullable 함수처럼
+// false이면 text값을 Some으로 감싸서 반환하고
+// true이면 None을 반환하는 함수를 만들어보자.
+
+export function fromSnakeCase(text: string) {
+  const snakeCaseRegex = /^[a-z0-9]+(_[a-z0-9]+)*$/;
+  // return snakeCaseRegex.test(text);
+  return snakeCaseRegex.test(text) ? O.some(text) : O.none
+}
--- src/is-snake-case/is-snake-case.test.ts
+++ src/is-snake-case/is-snake-case.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from 'vitest';
+import * as O from 'fp-ts/Option'
 import { isSnakeCase } from './is-snake-case.ts';
+import { fromSnakeCase } from './is-snake-case.fpts.ts';
 
 describe('isSnakeCase', () => {
     it('should return true for valid snake case strings', () => {
@@ -12,3 +14,15 @@ describe('isSnakeCase', () => {
         expect(isSnakeCase('ThisIsNotSnakeCase')).toBe(false);
     });
 });
+
+describe('fromSnakeCase', () => {
+    it('should return None for invalid snake case strings', () => {
+        expect(fromSnakeCase('ThisIsNotSnakeCase')).toEqual(O.none);
+    });
+
+    it('should return Some for valid snake case strings', () => {
+        expect(fromSnakeCase('this_is_snake_case')).toEqual(O.some('this_is_snake_case'));
+        expect(fromSnakeCase('snake_case')).toEqual(O.some('snake_case'));
+        expect(fromSnakeCase('snake_case123')).toEqual(O.some('snake_case123'));
+    });
+})
